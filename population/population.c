922950a9e63bace0ba321686b056eaf6163f2835
Stachu9 <142898133+Stachu9@users.noreply.github.com>
2023-08-22 15:29:16 +0000

    Tue, Aug 22, 2023, 5:29 PM +02:00

--- population/population.c
+++ population/population.c
@@ -16,10 +16,10 @@ int main(void)
     int numberYears = 0;
     int tempNumber = startSize;
 
-    while (tempNumber < endSize)
+    while (tempNumber < endSize);
     {
-        tempNumber += int(tempNumber/3);
-        tempNumber -= int(tempNumber/4);
+        tempNumber += tempNumber/3;
+        tempNumber -= tempNumber/4;
         numberYears ++;
     }
 
