d230c94782f839a938f110cf43b783f13164fee0
Stachu9 <142898133+Stachu9@users.noreply.github.com>
2023-08-22 15:16:00 +0000

    Tue, Aug 22, 2023, 5:16 PM +02:00

--- population/population.c
+++ population/population.c
@@ -5,9 +5,25 @@ int main(void)
 {
     // TODO: Prompt for start size
 
+    int startSize = get_int("Tell start size: ");
+
     // TODO: Prompt for end size
+
+    int endSize = get_int("Tell end size: ");
 
     // TODO: Calculate number of years until we reach threshold
 
+    int numberYears = 0;
+    int tempNumber = startSize;
+
+    while (tempNumber < endSize)
+    {
+        tempNumber += int(tempNumber/3);
+        tempNumber -= int(tempNumber/4);
+        numberYears += 1;
+    }
+
     // TODO: Print number of years
+    printf("Number of years is: %i/n", numberYears);
+
 }
