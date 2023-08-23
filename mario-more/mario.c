7012d8b2becc17a247269a509960e5206dd24b91
Stachu9 <142898133+Stachu9@users.noreply.github.com>
2023-08-23 00:09:46 +0000

    Wed, Aug 23, 2023, 2:09 AM +02:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -4,35 +4,34 @@
 int main(void)
 {
     int x;
-  do
-  {
-    x = get_int("Tell hieght: ");
-  }
-  while (x < 1 || x > 8);
-
-  int t = 1;
-  int b = x;
-
-  for(int i = 0; i < x; i++)
-  {
-    for(int j = 1; j < b; j++)
+    do
     {
-        printf(" ");
+        x = get_int("Tell hieght: ");
     }
-    for (int z = 0; z < t; z++)
+    while (x < 1 || x > 8);
+
+    int t = 1;
+    int b = x;
+
+    for (int i = 0; i < x; i++)
     {
-        printf("#");
+        for (int j = 1; j < b; j++)
+        {
+            printf(" ");
+        }
+        for (int z = 0; z < t; z++)
+        {
+            printf("#");
+        }
+        printf("  ");
+        for (int z = 0; z < t; z++)
+        {
+            printf("#");
+        }
+
+        printf("\n");
+
+        t++;
+        b--;
     }
-    printf("  ");
-    for (int z = 0; z < t; z++)
-    {
-        printf("#");
-    }
-
-    printf("\n");
-
-    t++;
-    b--;
-
-  }
 }
