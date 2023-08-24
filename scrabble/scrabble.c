f5a8afa213536051dc281dcadf2a2116d3b01506
Stachu9 <142898133+Stachu9@users.noreply.github.com>
2023-08-24 23:34:41 +0000

    Fri, Aug 25, 2023, 1:34 AM +02:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -45,6 +45,18 @@ string trim(string word)
     int lenght = strlen(word);
     for (int i = 0; i < lenght; i++)
     {
-        if (word[i])
+        if ((word[i] >= 65 && word[i] <= 90) || (word[i] >= 97 && word[i] <= 122))
+        {
+            counter++
+        }
+    }
+    char trimmed[counter];
+
+    for (int i = 0; i < lenght; i++)
+    {
+        if ((word[i] >= 65 && word[i] <= 90) || (word[i] >= 97 && word[i] <= 122))
+        {
+            counter++
+        }
     }
 }
