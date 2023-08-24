ad7a501e42b12a0064443e2ee9cb09bf6151d2b3
Stachu9 <142898133+Stachu9@users.noreply.github.com>
2023-08-24 13:27:52 +0000

    Thu, Aug 24, 2023, 3:27 PM +02:00

---
+++ array_lecture/alpha.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<cs50.h>
+
+int main(void)
+{
+    string word;
+    do
+    {
+        word = get_int("Word: ");
+    }
+    while(length < 1);
+
+    int arr[length];
+    arr[0] = 1;
+    printf("%i\n", arr[0]);
+
+    for (int i = 1; i < length; i++)
+    {
+        arr[i] = arr[i-1] * 2;
+        printf("%i\n", arr[i]);
+    }
+
+
+}
