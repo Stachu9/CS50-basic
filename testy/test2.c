c0d981ea7a95f2b20bd3ba1829f25e2417f46fe7
Stachu9 <142898133+Stachu9@users.noreply.github.com>
2023-08-24 23:41:17 +0000

    Fri, Aug 25, 2023, 1:41 AM +02:00

--- testy/test2.c
+++ testy/test2.c
@@ -1,17 +1,42 @@
+#include <ctype.h>
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
+
+string trim(string word);
 
 int main(void)
 {
     string s = "duuuupa";
-    char t = s[2] - 5;
 
-    if (t == 'p')
+    string b = trim(s);
+
+    printf("%s\n", b);
+
+}
+
+string trim(string word)
+{
+    int counter;
+    int lenght = strlen(word);
+    for (int i = 0; i < lenght; i++)
     {
-        printf("enter\n");
+        if ((word[i] >= 65 && word[i] <= 90) || (word[i] >= 97 && word[i] <= 122))
+        {
+            counter++;
+        }
+    }
+    char trimmed[counter];
+    int arrCounter = 0;
+
+    for (int i = 0; i < lenght; i++)
+    {
+        if ((word[i] >= 65 && word[i] <= 90) || (word[i] >= 97 && word[i] <= 122))
+        {
+            trimmed[arrCounter] = word[i];
+            arrCounter++;
+        }
     }
 
-
-    printf("%c\n", t);
-
+    return trimmed;
 }
