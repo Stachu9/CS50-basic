4d6edb68bbfaa397468243bbb39a54c8fd28f087
Stachu9 <142898133+Stachu9@users.noreply.github.com>
2023-08-25 13:39:12 +0000

    Fri, Aug 25, 2023, 3:39 PM +02:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -5,5 +5,14 @@
 
 int main(int argc, string argv[])
 {
+    if (argc < 2 || argc > 2)
+    {
+        printf("Usage: %i\n", argv[0])
+    }
+    do
+    {
+        string plainText = get_string(plainText);
+    }
+    while(!plainText);
 
 }
