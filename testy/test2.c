9101746f394d7870a4ebb644ec727bba6888b654
Stachu9 <142898133+Stachu9@users.noreply.github.com>
2023-08-25 00:55:26 +0000

    Fri, Aug 25, 2023, 2:55 AM +02:00

--- testy/test2.c
+++ testy/test2.c
@@ -3,16 +3,51 @@
 #include <stdio.h>
 #include <string.h>
 
-
+string trim(string word);
 
 int main(void)
 {
     string s = "duuuupa";
-    string t = "aad";
+    string t = trim(s);
 
-//nie wiem czemu, ale do tej linii zmienna t ma wartosc "duuuupa",
-// a w momencie jak wchodzi do print, to zmienna t sie zeruje i nic nie grukuje do konsoli.
     printf("%s\n", t);
 
 }
 
+//return only actual letters from word
+string trim(string word)
+{
+    //count actual letters in word
+    int counter = 0;
+    int lenght = strlen(word);
+    for (int i = 0; i < lenght; i++)
+    {
+        if ((word[i] >= 65 && word[i] <= 90) || (word[i] >= 97 && word[i] <= 122))
+        {
+            counter++;
+        }
+    }
+
+    //create array of only actual letters
+    char trimmed[counter];
+    int arrCounter = 0;
+
+    for (int i = 0; i < lenght; i++)
+    {
+        if ((word[i] >= 65 && word[i] <= 90) || (word[i] >= 97 && word[i] <= 122))
+        {
+            trimmed[arrCounter] = word[i];
+            arrCounter++;
+        }
+    }
+
+//jak tutaj dodalem printa, ktory drukuje cokolwiek, to tamten print w MAIN,
+// ktory wczesniej dzialal zle, teraz dziala dobrze.
+// nic wiecej nie zmienialem, tylko tutaj dodalem printa ktory drukuje cokolwiek.
+    printf("cokolwiek\n");
+
+    string p = trimmed;
+
+    // return string of actual letters
+    return p;
+}
