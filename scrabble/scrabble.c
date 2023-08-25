2d8e23d2c7e0af1252616d3caa8d6cdc09db23ef
Stachu9 <142898133+Stachu9@users.noreply.github.com>
2023-08-25 10:12:12 +0000

    Fri, Aug 25, 2023, 12:12 PM +02:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -1,5 +1,5 @@
-#include <ctype.h>
 #include <cs50.h>
+#include <ctype.h>
 #include <stdio.h>
 #include <string.h>
 
@@ -7,7 +7,6 @@
 int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
 
 int compute_score(string word);
-
 
 int main(void)
 {
