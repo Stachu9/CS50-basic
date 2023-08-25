3777d843e1990ec3a61be580bbc8b6b9b9fb18be
Stachu9 <142898133+Stachu9@users.noreply.github.com>
2023-08-25 00:50:18 +0000

    Fri, Aug 25, 2023, 2:50 AM +02:00

--- testy/test2.c
+++ testy/test2.c
@@ -3,12 +3,12 @@
 #include <stdio.h>
 #include <string.h>
 
-string trim(string word);
+
 
 int main(void)
 {
     string s = "duuuupa";
-    string t = trim(s);
+    string t = "aad";
 
 //nie wiem czemu, ale do tej linii zmienna t ma wartosc "duuuupa",
 // a w momencie jak wchodzi do print, to zmienna t sie zeruje i nic nie grukuje do konsoli.
@@ -16,34 +16,3 @@ int main(void)
 
 }
 
-string trim(string word)
-{
-    //count actual letters in word
-    int counter = 0;
-    int lenght = strlen(word);
-    for (int i = 0; i < lenght; i++)
-    {
-        if ((word[i] >= 65 && word[i] <= 90) || (word[i] >= 97 && word[i] <= 122))
-        {
-            counter++;
-        }
-    }
-
-    //create array of only actual letters
-    char trimmed[counter];
-    int arrCounter = 0;
-
-    for (int i = 0; i < lenght; i++)
-    {
-        if ((word[i] >= 65 && word[i] <= 90) || (word[i] >= 97 && word[i] <= 122))
-        {
-            trimmed[arrCounter] = word[i];
-            arrCounter++;
-        }
-    }
-
-    string p = trimmed;
-
-    // return array of actual letters
-    return p;
-}
