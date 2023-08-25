cefaf67164e004d764a7d5b9f9d9e9cd27fb0bbf
Stachu9 <142898133+Stachu9@users.noreply.github.com>
2023-08-25 11:16:08 +0000

    Fri, Aug 25, 2023, 1:16 PM +02:00

--- readability/readability.c
+++ readability/readability.c
@@ -4,6 +4,7 @@
 
 int count_letters(string text);
 int count_words(string text);
+int count_sentences(string text);
 
 int main(void)
 {
@@ -16,6 +17,7 @@ int main(void)
 
     printf("%i\n", count_letters(text));
     printf("%i\n", count_words(text));
+    printf("%i\n", count_sentences(text));
 
 }
 
@@ -54,4 +56,20 @@ int count_words(string text)
     }
     return wordsCounter;
 
+}
+
+int count_sentences(string text)
+{
+    int length = strlen(text);
+    int centenceCounter = 1;
+
+    for (int i = 0; i < length; i++)
+    {
+        if (text[i] == 33 || text[i] == 46 || text[i] == 63)
+        {
+            centenceCounter++;
+        }
+
+    }
+    return centenceCounter;
 }
