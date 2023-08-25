9805d8917a15a9ebcf1657efe2cbfde187ef28bc
Stachu9 <142898133+Stachu9@users.noreply.github.com>
2023-08-25 10:52:58 +0000

    Fri, Aug 25, 2023, 12:52 PM +02:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,7 +1,15 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+int count_letters(string text);
+
+int main(void)
 {
 
+}
+
+int count_letters(string text)
+{
+    int counter = 0;
+    
 }
