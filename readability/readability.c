7925c5d115c71fd46ae29722febcef5df0df73fb
Stachu9 <142898133+Stachu9@users.noreply.github.com>
2023-08-25 11:34:28 +0000

    Fri, Aug 25, 2023, 1:34 PM +02:00

--- readability/readability.c
+++ readability/readability.c
@@ -21,13 +21,25 @@ int main(void)
     printf("%i\n", count_sentences(text));
 
     float l = (count_letters(text) / count_words(text)) * 100;
-    float s = (count_sentences(text) / count_words(text))) * 100;
+    float s = (count_sentences(text) / count_words(text)) * 100;
     float grade = 0.0588 * l - 0.296 * s - 15.8;
 
     if (grade >= 1 && grade <= 16)
     {
-        printf("Grade %i" )
+        printf("Grade %+.0f\n", roundf(grade));
+
     }
+    else if (grade < 1)
+    {
+        printf("Before Grade 1\n");
+
+    }
+    else
+    {
+        printf("Grade 16+\n");
+
+    }
+
 
 }
 
