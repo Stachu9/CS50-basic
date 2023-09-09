578d9e5a4fd0faa00fb9373921de48d9fe1461d0
Stachu9 <142898133+Stachu9@users.noreply.github.com>
2023-09-09 11:14:46 +0000

    Sat, Sep 9, 2023, 1:14 PM +02:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -11,7 +11,7 @@ int properKeyLength = 26;
 int main(int argc, string argv[])
 {
 
-    if (argc < 2 || argc > 2)
+    if (argc !== 2 )
     {
         printf("Usage: %s\n", argv[0]);
         return 1;
