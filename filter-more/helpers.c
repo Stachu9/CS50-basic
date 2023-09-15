7d4c9c0dc04e13eda86d1b37c5d1ff13b6b52fd1
Stachu9 <142898133+Stachu9@users.noreply.github.com>
2023-09-15 21:41:34 +0000

    Fri, Sep 15, 2023, 11:41 PM +02:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -20,6 +20,13 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            image[i][-(j + 1)] = image[i][j];
+        }
+    }
     return;
 }
 
