82af7f9a04a3fae3f2940e777e84b5274535310f
Stachu9 <142898133+Stachu9@users.noreply.github.com>
2023-09-15 21:37:05 +0000

    Fri, Sep 15, 2023, 11:37 PM +02:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -4,11 +4,14 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
-    for (int i = 0; i < hieght; i++)
+    for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
         {
-            int avarage = round((image[i][j].rgbtBlue + ))
+            int avarage = round((image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed)/3);
+            image[i][j].rgbtBlue = avarage;
+            image[i][j].rgbtGreen = avarage;
+            image[i][j].rgbtRed = avarage;
         }
     }
     return;
