92947b11543b038baa7ba0ef562dc382255867ca
Stachu9 <142898133+Stachu9@users.noreply.github.com>
2023-09-15 22:06:14 +0000

    Sat, Sep 16, 2023, 12:06 AM +02:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -37,6 +37,16 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            for (int z = i - 1; z <= i + 1; z++)
+            {
+                for (int x = j - 1; x <= j + 1; x++)
+            }
+        }
+    }
     return;
 }
 
