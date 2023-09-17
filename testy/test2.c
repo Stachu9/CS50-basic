e26288e492ebae4c1bee9911b9738174afe8856e
Stachu9 <142898133+Stachu9@users.noreply.github.com>
2023-09-17 12:58:17 +0000

    Sun, Sep 17, 2023, 2:58 PM +02:00

--- testy/test2.c
+++ testy/test2.c
@@ -2,51 +2,13 @@
 #include <cs50.h>
 #include <stdio.h>
 #include <string.h>
-
-string trim(string word);
+#include <math.h>
 
 int main(void)
 {
-    string s = "duuu(*&*^*&upa";
-    string t = trim(s);
+    int avarage = round((27 + 28 + 28)/3);
 
-//nie wiem czemu, ale do tej linii zmienna t ma wartosc "duuuupa",
-// a w momencie jak wchodzi do print, to zmienna t sie zeruje
-// i nic nie drukuje.
+    printf("%i\n", avarage);
 
-    printf("%s\n", t);
-
-}
-
-//return only actual letters from word
-string trim(string word)
-{
-    //count actual letters in word
-    int counter = 0;
-    int lenght = strlen(word);
-    for (int i = 0; i < lenght; i++)
-    {
-        if ((word[i] >= 65 && word[i] <= 90) || (word[i] >= 97 && word[i] <= 122))
-        {
-            counter++;
-        }
-    }
-
-    //create array of only actual letters
-    char trimmed[counter];
-    int arrCounter = 0;
-
-    for (int i = 0; i < lenght; i++)
-    {
-        if ((word[i] >= 65 && word[i] <= 90) || (word[i] >= 97 && word[i] <= 122))
-        {
-            trimmed[arrCounter] = word[i];
-            arrCounter++;
-        }
-    }
-
-    string p = trimmed;
-
-    // return string of actual letters
-    return p;
+    return 0;
 }
