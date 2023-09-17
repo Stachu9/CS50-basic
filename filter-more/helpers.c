c76ef8fbc773e88c70bc05881330d2a582ab3f30
Stachu9 <142898133+Stachu9@users.noreply.github.com>
2023-09-17 11:09:52 +0000

    Sun, Sep 17, 2023, 1:09 PM +02:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -104,5 +104,65 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
 // Detect edges
 void edges(int height, int width, RGBTRIPLE image[height][width])
 {
+    RGBTRIPLE tempImage[height][width];
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            int zMin = i - 1;
+            if (zMin < 0)
+            {
+                zMin = 0;
+            }
+
+            int zMax = i + 1;
+            if (zMax >= height)
+            {
+                zMax = height - 1;
+            }
+
+            int xMin = j - 1;
+            if (xMin < 0)
+            {
+                xMin = 0;
+            }
+
+            int xMax = j + 1;
+            if (xMax >= width)
+            {
+                xMax = width - 1;
+            }
+
+            int counter = 0;
+
+            int avRed = 0;
+            int avGreen = 0;
+            int avBlue = 0;
+
+            for (int z = zMin; z <= zMax; z++)
+            {
+                for (int x = xMin; x <= xMax; x++)
+                {
+                    avRed += image[z][x].rgbtRed;
+                    avGreen += image[z][x].rgbtGreen;
+                    avBlue += image[z][x].rgbtBlue;
+                    counter ++;
+                    //printf("coordinates: %i, %i. Blue: %i, avBlue: %i, Counter: %i\n", z, x, image[z][x].rgbtBlue, avBlue, counter);
+                }
+            }
+            tempImage[i][j].rgbtRed = round(avRed / counter);
+            tempImage[i][j].rgbtGreen = round(avGreen / counter);
+            tempImage[i][j].rgbtBlue = round(avBlue / counter);
+        }
+    }
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            image[i][j] = tempImage[i][j];
+        }
+    }
     return;
 }
