35666a12f772dc1e349e4ebd608be7e10de751ed
Stachu9 <142898133+Stachu9@users.noreply.github.com>
2023-09-23 22:01:19 +0000

    Sun, Sep 24, 2023, 12:01 AM +02:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -73,7 +73,7 @@ bool load(const char *dictionary)
 unsigned int size(void)
 {
     // TODO
-    return 0;
+    return wordCounter;
 }
 
 // Unloads dictionary from memory, returning true if successful, else false
