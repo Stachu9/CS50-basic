138bc590d7cdd88fb4710126c148d3868dbd977b
Stachu9 <142898133+Stachu9@users.noreply.github.com>
2023-09-24 21:54:49 +0000

    Sun, Sep 24, 2023, 11:54 PM +02:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -1,36 +1,45 @@
-flag = True
-numberList = []
+def main():
+    flag = True
+    numberList = []
 
-while flag:
-    cardNum = input("Enter card number: ")
-    if cardNum:
-        flag = False
-    i = 0
-    for c in cardNum:
+    while flag:
+        cardNum = input("Enter card number: ")
+        if cardNum:
+            flag = False
+        i = 0
+        for c in cardNum:
+            try:
+                numberList[i] = int(c)
+                i += 1
+            except:
+                print("Number must contain only integers!")
+                flag = True
+                break
+
+    flag1 = True
+    counter = -1
+    sumTwice = 0
+    sumOnce = 0
+    while True:
         try:
-            numberList[i] = int(c)
-            i += 1
+            if flag1:
+                sumOnce += numberList[counter]
+                counter -= 1
+                flag = False
+            else:
+                sumTwice += numberList[counter] * 2
+                counter -= 1
+                flag = True
         except:
-            print("Number must contain only integers!")
-            flag = True
             break
 
-flag1 = True
-counter = -1
-sumTwice = 0
-sumOnce = 0
-while True:
-    try:
-        if flag1:
-            sumOnce += numberList[counter]
-            counter -= 1
-            flag = False
-        else:
-            sumTwice += numberList[counter] * 2
-            counter -= 1
-            flag = True
-    except:
-        break
+    if not ((sumOnce + sumTwice) % 10 == 0):
+        print("INVALID")
+        return
+
+    
+
+
 
 
 
