1d410121abef5dab6c228db06f2b7d96f858edc9
Stachu9 <142898133+Stachu9@users.noreply.github.com>
2023-09-24 23:01:02 +0000

    Mon, Sep 25, 2023, 1:01 AM +02:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -2,3 +2,14 @@ text = ""
 
 while not text:
     text = input("Text: ")
+
+sentences = 0
+words = 0
+letters = 0
+for c in text:
+    if c == "." or c == "!" or c == "?":
+        sentences += 1
+    elif c == " ":
+        words += 1
+    elif isalpha(c):
+        
