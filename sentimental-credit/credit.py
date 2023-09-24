539e2f056176ac2e8e5f0df69db653762db6e7d3
Stachu9 <142898133+Stachu9@users.noreply.github.com>
2023-09-24 21:40:06 +0000

    Sun, Sep 24, 2023, 11:40 PM +02:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -1,15 +1,20 @@
 flag = True
+numberList = []
+
 while flag:
     cardNum = input("Enter card number: ")
     if cardNum:
         flag = False
+    i = 0
     for c in cardNum:
         try:
-            test = int(c)
+            numberList[i] = int(c)
+            i += 1
         except:
             print("Number must contain only integers!")
             flag = True
             break
 
+numbe
 
 
