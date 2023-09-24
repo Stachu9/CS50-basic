6b9bf604d851f930757291039e5cf75d7cf431fb
Stachu9 <142898133+Stachu9@users.noreply.github.com>
2023-09-24 22:35:54 +0000

    Mon, Sep 25, 2023, 12:35 AM +02:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -1,6 +1,6 @@
 def main():
     flag = True
-    numberList = []
+    numList = []
 
     while flag:
         cardNum = input("Enter card number: ")
@@ -9,7 +9,7 @@ def main():
         i = 0
         for c in cardNum:
             try:
-                numberList.append(int(c))
+                numList.append(int(c))
                 i += 1
             except:
                 print("Number must contain only integers!")
@@ -23,11 +23,11 @@ def main():
     while True:
         try:
             if flag1:
-                sumOnce += numberList[counter]
+                sumOnce += numList[counter]
                 counter -= 1
                 flag1 = False
             else:
-                for digit in str(numberList[counter] * 2):
+                for digit in str(numList[counter] * 2):
                     sumTwice += int(digit)
                 counter -= 1
                 flag1 = True
@@ -36,16 +36,16 @@ def main():
 
     if ((sumOnce + sumTwice) % 10 == 0):
         if (numList[0] == 4 and (len(numList) == 13 or len(numList) == 16)):
-            printf("VISA\n");
+            print("VISA\n");
             return
         if numList[0] == 3 and (numList[1] == 4 or numList[1] == 7) and (len(numList) == 15):
-            printf("AMEX\n");
+            print("AMEX\n");
             return
         if (numList[0] == 5 and (numList[1] == 1 or numList[1] == 2 or numList[1] == 3 or numList[1] == 4 or numList[1] == 5) and len(numList) == 16):
-            printf("MASTERCARD\n");
+            print("MASTERCARD\n");
             return
     else:
-        print("INVALID")
+        print("INVALID\n")
         return
 
 main()
