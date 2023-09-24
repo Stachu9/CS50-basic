80a8154214da19b910065f651776e75a24c6ff4c
Stachu9 <142898133+Stachu9@users.noreply.github.com>
2023-09-24 23:42:04 +0000

    Mon, Sep 25, 2023, 1:42 AM +02:00

--- dna/dna.py
+++ dna/dna.py
@@ -21,6 +21,8 @@ def main():
             counter += 1
 
     # TODO: Read DNA sequence file into a variable
+    with open(sys.atg[2]) as file:
+        text = file.read()
 
     # TODO: Find longest match of each STR in DNA sequence
 
