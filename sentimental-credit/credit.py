0abd9943a47fb92f2ca32d12b4df519735663b5e
Stachu9 <142898133+Stachu9@users.noreply.github.com>
2023-09-24 22:10:33 +0000

    Mon, Sep 25, 2023, 12:10 AM +02:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -34,13 +34,13 @@ def main():
             break
 
     if ((sumOnce + sumTwice) % 10 == 0):
-        if (numList[0] == 4 and (len(numList) == 13 or len(numList) == 16))
+        if (numList[0] == 4 and (len(numList) == 13 or len(numList) == 16)):
             printf("VISA\n");
             return
-        if (numList[0] == 3 and (numList[1] == 4 or numList[1] == 7) and (len(numList) == 15)
+        if numList[0] == 3 and (numList[1] == 4 or numList[1] == 7) and (len(numList) == 15):
             printf("AMEX\n");
             return
-        if (numList[0] == 5 and (numList[1] == 1 or numList[1] == 2 or numList[1] == 3 or numList[1] == 4 or numList[1] == 5) and len(numList) == 16)
+        if (numList[0] == 5 and (numList[1] == 1 or numList[1] == 2 or numList[1] == 3 or numList[1] == 4 or numList[1] == 5) and len(numList) == 16):
             printf("MASTERCARD\n");
             return
     else:
