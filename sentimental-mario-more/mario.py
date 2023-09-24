c37b0ec3864a583b4aa466efe8add2f07376ff00
Stachu9 <142898133+Stachu9@users.noreply.github.com>
2023-09-24 20:12:26 +0000

    Sun, Sep 24, 2023, 10:12 PM +02:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -5,14 +5,14 @@ while flag:
         flag = False
 
 for i in range(height):
-    for j in range(height - i+1):
+    for j in range(height - (i+1)):
         print(" ", end = "")
 
     for z in range(i+1):
         print("#", end = "")
 
     print("  ", end = "")
-    
+
     for z in range(i+1):
         print("#", end = "")
     print()
