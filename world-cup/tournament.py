d0f4a2a01fd0a3863d6b972efcffed2e6c9b156f
Stachu9 <142898133+Stachu9@users.noreply.github.com>
2023-09-24 16:57:50 +0000

    Sun, Sep 24, 2023, 6:57 PM +02:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -17,12 +17,13 @@ def main():
     teams = []
     # TODO: Read teams into memory from file
     with open(sys.argv[1]) as file:
-        file_reader = DictReader(file)
+        file_reader = csv.DictReader(file)
 
     counter = 0
     for team in file_reader:
         teams += [team]
         teams[counter][rating] = int(teams[counter][rating])
+        print(teams[counter])
 
     counts = {}
     # TODO: Simulate N tournaments and keep track of win counts
