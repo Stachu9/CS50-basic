4ef381cc23f04678d8ddc9ba6523e45f379af776
Stachu9 <142898133+Stachu9@users.noreply.github.com>
2023-09-29 20:48:44 +0000

    Fri, Sep 29, 2023, 10:48 PM +02:00

--- movies/7.sql
+++ movies/7.sql
@@ -1,1 +1,1 @@
-SELECT title FROM (SELECT * FROM ratings JOIN movies ON ratings.movie_id = movies.id)WHERE YEAR = 2010 ORDER BY rating DESC, title;
+SELECT title, rating FROM (SELECT * FROM ratings JOIN movies ON ratings.movie_id = movies.id)WHERE YEAR = 2010 ORDER BY rating DESC, title;
