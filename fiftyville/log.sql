d89e7b9650ac46d1a7a9dcd8652b5622d6871e50
Stachu9 <142898133+Stachu9@users.noreply.github.com>
2023-09-30 21:31:56 +0000

    Sat, Sep 30, 2023, 11:31 PM +02:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -11,7 +11,7 @@ SELECT person_id FROM bank_accounts JOIN (SELECT account_number, amount FROM atm
 SELECT name, phone_number, passport_number, license_plate FROM people JOIN (SELECT person_id, creation_year, amount FROM bank_accounts JOIN (SELECT account_number, amount FROM atm_transactions WHERE year = 2021 AND month = 7 AND day = 28 AND atm_location = "Leggett Street" AND transaction_type = "withdraw") AS x ON bank_accounts.account_number = x.account_number) AS z ON people.id = z.person_id;
 -- !!Check logs from bakery parking
 SELECT license_plate FROM bakery_security_logs WHERE year = 2021 AND month = 7 AND day = 28 AND hour = 10 AND minute <= 25 AND minute >= 15 AND activity = "exit";
--- Check phone calls
+-- !!Check phone calls
 SELECT caller, receiver FROM phone_calls WHERE year = 2021 AND month = 7 AND day = 28 AND duration < 60;
 -- Checkk Fiftyville airport id
 SELECT id FROM airports WHERE full_name LIKE "%iftyville%";
