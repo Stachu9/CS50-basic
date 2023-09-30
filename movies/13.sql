bba8e07fc8c850b588167433080211e77dbf22d9
Stachu9 <142898133+Stachu9@users.noreply.github.com>
2023-09-30 19:26:50 +0000

    Sat, Sep 30, 2023, 9:26 PM +02:00

--- movies/13.sql
+++ movies/13.sql
@@ -1,1 +1,1 @@
-SELECT * FROM stars JOIN (SELECT * FROM movies JOIN stars ON movies.id = stars.movie_id WHERE person_id = (SELECT id FROM people WHERE name = "Kevin Bacon" AND birth = 1958)) ON stars.movie_id = movies.id;
+SELECT * FROM stars JOIN (SELECT * FROM movies JOIN stars ON movies.id = stars.movie_id WHERE person_id = (SELECT id FROM people WHERE name = "Kevin Bacon" AND birth = 1958)) AS dupa ON stars.movie_id = dupa.id;
