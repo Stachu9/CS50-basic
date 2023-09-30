94b8dc0576df0e628ba72d17c7ce5efe52e09305
Stachu9 <142898133+Stachu9@users.noreply.github.com>
2023-09-30 20:32:50 +0000

    Sat, Sep 30, 2023, 10:32 PM +02:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -3,3 +3,5 @@
 SELECT description FROM crime_scene_reports WHERE year = 2021 AND month = 7 AND day = 28 AND street = "Humphrey Street";
 -- Read witnesses interviews:
 SELECT name, transcript FROM interviews WHERE year = 2021 AND month = 7 AND day = 28 AND transcript LIKE "%bakery%";
+-- Check Legget Street ATM transaction before robbery
+SELECT account_number, amount FROM atm_transactions WHERE year = 2021 AND month = 7 AND day = 28 AND atm_location = "Legget Street" AND transaction_type = "withdraw";
