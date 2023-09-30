402fdcbaf4c0319b917f7eb94664a4b129388a3d
Stachu9 <142898133+Stachu9@users.noreply.github.com>
2023-09-30 18:44:36 +0000

    Sat, Sep 30, 2023, 8:44 PM +02:00

--- movies/12.sql
+++ movies/12.sql
@@ -1,2 +1,2 @@
-SELECT title FROM (SELECT * FROM movies JOIN stars ON movies.id = stars.movie_id JOIN people ON stars.person_id = people.id
-WHERE name = "Bradley Cooper" OR name = "Jennifer Lawrence");
+SELECT title, COUNT(title) FROM (SELECT * FROM movies JOIN stars ON movies.id = stars.movie_id JOIN people ON stars.person_id = people.id
+WHERE name = "Bradley Cooper" OR name = "Jennifer Lawrence") GROUP BY title;
