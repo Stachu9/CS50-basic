575ae62838827988f28292ba16318573eddee48e
Stachu9 <142898133+Stachu9@users.noreply.github.com>
2023-09-30 18:42:53 +0000

    Sat, Sep 30, 2023, 8:42 PM +02:00

--- movies/12.sql
+++ movies/12.sql
@@ -1,2 +1,2 @@
 SELECT title FROM (SELECT * FROM movies JOIN stars ON movies.id = stars.movie_id JOIN people ON stars.person_id = people.id
-WHERE name = "Bradley Cooper" OR name = "Jennifer Lawrence";
+WHERE name = "Bradley Cooper" OR name = "Jennifer Lawrence");
