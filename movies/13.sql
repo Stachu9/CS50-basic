086e3f6dfdf872eec36d090c65c618d8ea94914d
Stachu9 <142898133+Stachu9@users.noreply.github.com>
2023-09-30 18:57:32 +0000

    Sat, Sep 30, 2023, 8:57 PM +02:00

--- movies/13.sql
+++ movies/13.sql
@@ -1,1 +1,1 @@
-SELECT id FROM people WHERE name = "Kevin Bacon" AND birth = 1958;
+SELECT * FROM movies JOIN stars ON movies.id = stars.movie_id WHERE person_id = (SELECT id FROM people WHERE name = "Kevin Bacon" AND birth = 1958);
