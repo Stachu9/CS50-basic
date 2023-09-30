ff0c10907905831b17a4269c17bed2293350b524
Stachu9 <142898133+Stachu9@users.noreply.github.com>
2023-09-30 21:43:17 +0000

    Sat, Sep 30, 2023, 11:43 PM +02:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -22,4 +22,7 @@ Select city FROM airports WHERE id = (SELECT destination_airport_id FROM flights
 -- !!Check passangers on flight
 SELECT passport_number, seat FROM passengers WHERE flight_id = (SELECT id FROM flights WHERE origin_airport_id = (SELECT id FROM airports WHERE full_name LIKE "%iftyville%") AND year = 2021 AND month = 7 AND day = 29 ORDER BY hour, minute LIMIT 1);
 -- join ATM person with parking EXIT
-SELECT name, phone_number, passport_number FROM (SELECT name, phone_number, passport_number, license_plate FROM people JOIN (SELECT person_id, creation_year, amount FROM bank_accounts JOIN (SELECT account_number, amount FROM atm_transactions WHERE year = 2021 AND month = 7 AND day = 28 AND atm_location = "Leggett Street" AND transaction_type = "withdraw") AS x ON bank_accounts.account_number = x.account_number) AS z ON people.id = z.person_id;) AS x JOIN (SELECT license_plate FROM bakery_security_logs WHERE year = 2021 AND month = 7 AND day = 28 AND hour = 10 AND minute <= 25 AND minute >= 15 AND activity = "exit") AS y ON x.license_plate = y.license_plate;
+SELECT name, phone_number, passport_number FROM
+    (SELECT name, phone_number, passport_number, license_plate FROM people JOIN
+        (SELECT person_id, creation_year, amount FROM bank_accounts JOIN
+            (SELECT account_number, amount FROM atm_transactions WHERE year = 2021 AND month = 7 AND day = 28 AND atm_location = "Leggett Street" AND transaction_type = "withdraw") AS x ON bank_accounts.account_number = x.account_number) AS z ON people.id = z.person_id;) AS x JOIN (SELECT license_plate FROM bakery_security_logs WHERE year = 2021 AND month = 7 AND day = 28 AND hour = 10 AND minute <= 25 AND minute >= 15 AND activity = "exit") AS y ON x.license_plate = y.license_plate;
