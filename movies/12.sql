2e334832fb7bb60529a2f85481364e7fafb0fe13
Stachu9 <142898133+Stachu9@users.noreply.github.com>
2023-09-30 14:32:15 +0000

    Sat, Sep 30, 2023, 4:32 PM +02:00

--- movies/12.sql
+++ movies/12.sql
@@ -1,1 +1,1 @@
-SELECT title FROM movies JOIN stars ON movies.id = stars.movie_id JOIN people ON stars.person_id = people.id WHERE name = "Bradle
+SELECT title FROM movies JOIN stars ON movies.id = stars.movie_id JOIN people ON stars.person_id = people.id WHERE name = "Bradley Cooper"
