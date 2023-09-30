bbb3ef56d9474a3aa6b99c707442a5e27e8ed364
Stachu9 <142898133+Stachu9@users.noreply.github.com>
2023-09-30 20:54:35 +0000

    Sat, Sep 30, 2023, 10:54 PM +02:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -9,3 +9,5 @@ SELECT account_number, amount FROM atm_transactions WHERE year = 2021 AND month 
 SELECT person_id, creation_year, amount FROM bank_accounts JOIN (SELECT account_number, amount FROM atm_transactions WHERE year = 2021 AND month = 7 AND day = 28 AND atm_location = "Leggett Street" AND transaction_type = "withdraw") AS x ON bank_accounts.account_number = x.account_number;
 -- Check names, phone numbers, passports, lcense plates, account creation year who withdrawed money from ATM
 SELECT name, phone_number, passport_number, license_plate, creation_year, amount FROM people JOIN (SELECT person_id, creation_year, amount FROM bank_accounts JOIN (SELECT account_number, amount FROM atm_transactions WHERE year = 2021 AND month = 7 AND day = 28 AND atm_location = "Leggett Street" AND transaction_type = "withdraw") AS x ON bank_accounts.account_number = x.account_number) AS z ON people.id = z.person_id;
+-- Check logs from bakery parking
+SELECT license_plate, hour, minute FROM bakery_security_logs WHERE year = 2021 AND month = 7 AND day = 28 AND hour = 10 AND minute <= 25 AND minute >= 15 AND activity = "leave";
