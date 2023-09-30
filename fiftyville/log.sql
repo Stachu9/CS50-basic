e14b4ca22f8d60c5c9d8c9392c82ba72114e730c
Stachu9 <142898133+Stachu9@users.noreply.github.com>
2023-09-30 21:49:03 +0000

    Sat, Sep 30, 2023, 11:49 PM +02:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -27,5 +27,6 @@ SELECT name, phone_number, passport_number FROM
         (SELECT person_id, creation_year, amount FROM bank_accounts JOIN
             (SELECT account_number, amount FROM atm_transactions WHERE year = 2021 AND month = 7 AND day = 28 AND atm_location = "Leggett Street" AND transaction_type = "withdraw")
         AS x ON bank_accounts.account_number = x.account_number)
-    AS z ON people.id = z.person_id) AS x JOIN (SELECT license_plate FROM bakery_security_logs WHERE year = 2021 AND month = 7 AND day = 28 AND hour = 10 AND minute <= 25 AND minute >= 15 AND activity = "exit")
-AS y ON x.license_plate = y.license_plate;
+    AS z ON people.id = z.person_id) AS j JOIN
+    (SELECT license_plate FROM bakery_security_logs WHERE year = 2021 AND month = 7 AND day = 28 AND hour = 10 AND minute <= 25 AND minute >= 15 AND activity = "exit")
+AS y ON j.license_plate = y.license_plate;
