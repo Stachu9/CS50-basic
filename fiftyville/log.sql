c0ceca2d8e3b827455707d04dab3288c55cc0355
Stachu9 <142898133+Stachu9@users.noreply.github.com>
2023-09-30 20:16:03 +0000

    Sat, Sep 30, 2023, 10:16 PM +02:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,3 +1,5 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 -- Read crime scene reports:
 SELECT description FROM crime_scene_reports WHERE year = 2021 AND month = 7 AND day = 28 AND street = "Humphrey Street";
+-- Read witnesses interviews:
+SELECT name, transcript FROM interviews WHERE year = 2012 AND month = 7 AND day = 28 AND transcript LIKE "%bakery%";
