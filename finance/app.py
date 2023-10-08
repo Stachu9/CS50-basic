5b4e5d641855b92ca409c2716bc4393e49be6215
Stachu9 <142898133+Stachu9@users.noreply.github.com>
2023-10-08 21:31:39 +0000

    Sun, Oct 8, 2023, 11:31 PM +02:00

--- finance/app.py
+++ finance/app.py
@@ -114,7 +114,7 @@ def register():
         password = request.form.get("password")
         confirmation = request.form.get("confirmation")
 
-        userList = db.execute("SELECT COUNT() FROM ")
+        userList = db.execute("SELECT COUNT(*) FROM ")
 
         if not username
 
