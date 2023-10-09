ba6a6eb4c4df625e1368f2dfa96ebce6ee54d00d
Stachu9 <142898133+Stachu9@users.noreply.github.com>
2023-10-09 18:19:54 +0000

    Mon, Oct 9, 2023, 8:19 PM +02:00

--- finance/app.py
+++ finance/app.py
@@ -48,13 +48,13 @@ def buy():
         symbol = request.form.get("symbol")
         shares = request.form.get("shares")
         stackObject = lookup(symbol)
-        wallet = db.execute(SELECT cash FROM users WHERE id = ?)
+        wallet = db.execute("SELECT cash FROM users WHERE id = ?;", session["user_id"])
+        print(wallet)
 
         if symbol or stackObject:
             return apology("Invalid symbol", 400)
         if shares:
             return apology("missing shares", 400)
-        if shares
 
 
     else:
