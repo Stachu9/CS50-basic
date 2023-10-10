249a82735971f385aa072679def15efa279991ec
Stachu9 <142898133+Stachu9@users.noreply.github.com>
2023-10-10 21:25:36 +0000

    Tue, Oct 10, 2023, 11:25 PM +02:00

--- finance/app.py
+++ finance/app.py
@@ -207,7 +207,7 @@ def sell():
         symbol = request.form.get("symbol")
         sharesToSell = request.form.get("shares")
         price = lookup(symbol)["price"]
-        sharesInWallet = db.execute(SELECT SUM(num_shares) FROM transactions WHERE symbol = ? AND person_id = ?;", el["symbol"], session["user_id"]
+        sharesInWallet = db.execute(SELECT SUM(num_shares) FROM transactions WHERE symbol = ? AND person_id = ?;", symbol, session["user_id"]
 
         if
     else:
