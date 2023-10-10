91891cd80fbeb3ee68b7ea10b6c2ceb19697334a
Stachu9 <142898133+Stachu9@users.noreply.github.com>
2023-10-10 14:36:43 +0000

    Tue, Oct 10, 2023, 4:36 PM +02:00

--- finance/app.py
+++ finance/app.py
@@ -207,5 +207,5 @@ def sell():
     else:
         shares = db.execute("SELECT symbol FROM transactions WHERE person_id = ? GROUP BY symbol;", session["user_id"])
         for share in shares:
-            share["number"] = db.execute("SELECT SUM(share_num) FROM transactions WHERE person_id = ? AND symbol = ?;", session["user_id"], share["symbol"])
+            share["number"] = db.execute("SELECT SUM(num_share) FROM transactions WHERE person_id = ? AND symbol = ?;", session["user_id"], share["symbol"])
     return render_template("sell.html")
